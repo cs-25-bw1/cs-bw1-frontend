7e794dff281d43bb9bbb32de50f8768853a6c1f3
denmercs <denmercs@gmail.com>
2020-02-04 14:52:06 -0600

    login and registration work

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import AuthRoute from "./util/AuthRoute";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <AuthRoute path="/" component={Home} />
       <Switch>
         <Route path={"/login"} component={Login} />
+        <Route path={"/register"} component={Register} />
       </Switch>
     </div>
   );
