61b7d1e6070fd9d4451559859cdc7a427e5e9e63
Sean Doyle <sdoylelambda@gmail.com>
2020-02-07 08:29:41 -0500

    updating branch.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { getMap } from "../store/app/actions";
-import { connect } from "react-redux";
-import room_data from "../util/example-room";
+import React, { useState, useEffect } from 'react'
+import { getMap } from '../store/app/actions'
+import { connect } from 'react-redux'
+import room_data from '../util/example-room'
 import {
   XAxis,
   YAxis,
@@ -11,19 +11,20 @@ import {
   HorizontalGridLines,
   LineMarkSeries,
   LineSeries,
-  MarkSeries
-} from "react-vis";
+  MarkSeries,
+} from 'react-vis'
 
 const Map = props => {
+  console.log('map props', props)
   useEffect(() => {
-    props.getMap();
+    props.getMap()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   //   const getPosition = props.map;
   //   console.log("this is the map props", getPosition);
-  const coordinates = [];
-  const links = [];
+  const coordinates = []
+  const links = []
 
   // Loop through each room in the room_data object
   //   for (let room in room_data) {
@@ -36,13 +37,13 @@ const Map = props => {
   for (let room in room_data) {
     // Set data equal to the first element (x, y coordinates)
     // in each room of the room_data object
-    let data = room_data[room][0];
-    coordinates.push(data);
+    let data = room_data[room][0]
+    coordinates.push(data)
     for (let adjacentRoom in room_data[room][1]) {
       links.push([
         room_data[room][0],
-        room_data[room_data[room][1][adjacentRoom]][0]
-      ]);
+        room_data[room_data[room][1][adjacentRoom]][0],
+      ])
     }
   }
 
@@ -51,26 +52,36 @@ const Map = props => {
       <FlexibleXYPlot width={600} height={600}>
         {links.map(link => (
           <LineSeries
-            strokeWidth="2"
-            color="green"
+            strokeWidth='5'
+            color='green'
             data={link}
             key={Math.random() * 100}
           />
         ))}
         <MarkSeries
-          //   current={this.props.currentRoom}
-          highlight="#1b00ff"
+          // current={[43, 34]}
+          highlight='#1b00ff'
           strokeWidth={3}
-          opacity="1"
-          size="3"
-          color="green"
+          opacity='1'
+          size='5'
+          color='green'
           data={coordinates}
-          style={{ cursor: "pointer" }}
+          style={{ cursor: 'pointer' }}
+        />
+        <MarkSeries
+          // current={[4, 6]}
+          highlight='#1b00ff'
+          strokeWidth={3}
+          opacity='1'
+          size='5'
+          color='blue'
+          data={[8, 8]}
+          style={{ cursor: 'pointer' }}
         />
       </FlexibleXYPlot>
     </>
-  );
-};
+  )
+}
 
 /***
  *     {coordinates.map(location => {
@@ -85,8 +96,8 @@ const Map = props => {
 
 const mapStateToProps = state => {
   return {
-    ...state.map
-  };
-};
+    ...state.map,
+  }
+}
 
-export default connect(mapStateToProps, { getMap })(Map);
+export default connect(mapStateToProps, { getMap })(Map)
