10a405d41d26b908b9d3459ba8213741dfddb7c4
Sean Doyle <sdoylelambda@gmail.com>
2020-02-07 10:34:37 -0500

    still looping, think if else in the useEffect to change location.

--- src/components/World.js
+++ src/components/World.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { initWorld, movePlayer } from "../store/app/actions";
-import Controller from "./Controller";
-import Map from "./Map";
-import "./world.scss";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { initWorld, movePlayer } from '../store/app/actions'
+import Controller from './Controller'
+import Map from './Map'
+import './world.scss'
+import { Link } from 'react-router-dom'
 
 const World = props => {
   /** varaibles */
-  const { name, title, description, location, items, players } = props.player;
+  const { name, title, description, location, items, players } = props.player
 
   useEffect(() => {
-    props.initWorld();
+    props.initWorld()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   useEffect(() => {
-    movePlayer();
-  }, [props.player]);
+    movePlayer()
+  }, [props.player])
 
   return (
     <>
-      <div className="world">
-        <div className="map">
+      <div className='world'>
+        <div className='map'>
           <Map location={location} />
         </div>
-        <div className="side">
-          <div className="playerDiv">
+        <div className='side'>
+          <div className='playerDiv'>
             <h3>Player name: </h3>
             <p>{name}</p>
             <h3>Current Room</h3>
@@ -43,24 +43,24 @@ const World = props => {
           </div>
         </div>
       </div>
-      <div className="logoutDiv">
+      <div className='logoutDiv'>
         <Link
-          to="/"
-          className="logout"
-          onClick={() => localStorage.removeItem("token")}
+          to='/'
+          className='logout'
+          onClick={() => localStorage.removeItem('token')}
         >
           Logout
         </Link>
       </div>
     </>
-  );
-};
+  )
+}
 
 const mapStateToProps = state => {
   return {
     map: state.gameReducer.map,
-    player: state.gameReducer.player
-  };
-};
+    player: state.gameReducer.player,
+  }
+}
 
-export default connect(mapStateToProps, { initWorld })(World);
+export default connect(mapStateToProps, { initWorld })(World)
